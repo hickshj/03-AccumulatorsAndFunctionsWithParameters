61ec040caadc5ce64312abc63a8f6f02a611b3ca
hickshj <hickshj@rose-hulman.edu>
2018-09-09 14:09:01 -0400

    soowoo

--- src/m9_using_objects.py
+++ src/m9_using_objects.py
@@ -5,8 +5,8 @@ This module lets you practice  ** using objects **, including:
   -- accessing their DATA via INSTANCE VARIABLES
 
 Authors: David Mutchler, Dave Fisher, Vibha Alangar, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Hunter Hicks.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -14,6 +14,8 @@ import rosegraphics as rg
 def main():
     """ Calls the other functions to demonstrate and/or test them. """
     # Test your functions by putting calls to them here:
+    # two_circles()
+    circle_and_rectangle()
 
 
 def two_circles():
@@ -27,12 +29,23 @@ def two_circles():
     -- Waits for the user to press the mouse, then closes the window.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement this function, per its green doc-string above.
+    # Done: 2. Implement this function, per its green doc-string above.
     #    -- ANY two rg.Circle objects that meet the criteria are fine.
     #    -- File  COLORS.pdf  lists all legal color-names.
     # Put a statement in   main   to test this function
     #    (by calling this function).
     # ------------------------------------------------------------------
+    window = rg.RoseWindow(400, 400)
+    center1 = rg.Point(100, 100)
+    center2 = rg.Point(300, 300)
+    circle1 = rg.Circle(center1, 50)
+    circle1.fill_color = 'blue'
+    circle2 = rg.Circle(center2, 100)
+    circle1.attach_to(window)
+    circle2.attach_to(window)
+    window.render()
+    window.close_on_mouse_click()
+
 
 
 def circle_and_rectangle():
@@ -75,6 +88,18 @@ def circle_and_rectangle():
     # IMPORTANT: Use the DOT TRICK to guess the names of the relevant
     #       instance variables for outline thickness, etc.
     # ------------------------------------------------------------------
+    window = rg.RoseWindow(800, 800)
+    thick1 = 1
+    thick2 = 1
+    x1 = 100
+    y1 = 100
+    x2 = 300
+    y2 = 300
+    p1 = rg.Point(x1, y1)
+    col1 = 'blue'
+
+
+
 
 
 def lines():
